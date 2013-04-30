31bbfa610ab098cdfe1c215cdaba89d65bb03c71
parrot-studio <parrot.studio.dev@gmail.com>
2013-05-01 00:01:11 +0900

    Rails4のRC1に差し替え

--- Gemfile
+++ Gemfile
@@ -1,8 +1,9 @@
 source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.0.0.beta1'
+gem 'rails', '4.0.0.rc1'
 
+gem 'json'
 gem 'mysql2'
 gem 'settingslogic'
 gem 'dalli'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,4 +4,4 @@ session_params = {
   key: "_gagnrath_session_#{ServerSettings.app_path}"
 }
 session_params[:path] = ServerSettings.app_path unless ServerSettings.app_path.blank?
-Gagnrath::Application.config.session_store :encrypted_cookie_store, session_params
+Gagnrath::Application.config.session_store :cookie_store, session_params
