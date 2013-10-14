1543fcd284503eb564168ec4d67d8d44f561f0e4
parrot-studio <parrot.studio.dev@gmail.com>
2013-10-14 09:43:12 +0900

    ver1.0

--- bin/app/mail_env_test.rb
+++ bin/app/mail_env_test.rb
@@ -3,7 +3,7 @@ require 'optparse'
 
 options = {}
 opt = OptionParser.new
-opt.on('-e ENV', '--env=ENV', 'execute env, equal to PADRINO_ENV=env'){|v| options[:env] = v}
+opt.on('-e ENV', '--env=ENV', 'execute env, equal to RAILS_ENV=env'){|v| options[:env] = v}
 opt.parse!(ARGV)
 
 RAILS_ENV = options[:env] if options[:env]
